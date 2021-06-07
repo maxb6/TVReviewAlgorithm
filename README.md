8fd30c6c3c604666b5486985358f7df2f84e079c
ckareez <cgkarel123@gmail.com>
2021-06-07 12:31:43 -0400

    added imports

---
+++ README.md
@@ -0,0 +1,10 @@
+#COMP472 - Assignment2
+
+Programmed by:
+- Constantine Karellas - 40109253
+- Max Burah - 40077075
+
+Libraries Used:
+- math
+- numpy
+- random
--- main.py
+++ main.py
@@ -7,7 +7,29 @@ import math
 import string
 import numpy as np
 import random
+from sklearn.feature_extraction.text import *
+from sklearn.datasets import load_files
+from sklearn import *
+from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import *
+from collections import Counter
+import matplotlib.pyplot as plt
+import re
+import pandas as pd
+
+def read_documents(docName):
+    docs = []
+    label = []
+    with open(docName, encoding="utf8") as f:
+        for line in f:
+            split_line = re.findall(r'\w+', line)
+            label.append(split_line[1])
+            docs.append(" ".join(split_line[4:]))
+    return docs, label
 
 
 
 
+
+
+print("\nProgram Terminated.\n")
