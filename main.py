fdc6aa3c1f20eb71f7972843f0d5813c78a075d0
ckareez <cgkarel123@gmail.com>
2021-06-10 15:09:49 -0400

    web scraping from imdb

--- main.py
+++ main.py
@@ -3,33 +3,37 @@
 # Constantine Karellas - 40109253
 # Max Burah - 40077075
 
-import math
-import string
-import numpy as np
-import random
-from sklearn.feature_extraction.text import *
-from sklearn.datasets import load_files
-from sklearn import *
-from sklearn.model_selection import train_test_split
-from sklearn.naive_bayes import *
-from collections import Counter
-import matplotlib.pyplot as plt
-import re
+from matplotlib.pyplot import cla
 import pandas as pd
+import requests
+from bs4 import BeautifulSoup
+import numpy as np
 
-def read_documents(docName):
-    docs = []
-    label = []
-    with open(docName, encoding="utf8") as f:
-        for line in f:
-            split_line = re.findall(r'\w+', line)
-            label.append(split_line[1])
-            docs.append(" ".join(split_line[4:]))
-    return docs, label
+url = 'https://www.imdb.com/title/tt0098904/episodes?season=1&ref_=ttep_ep_sn_pv'
+response = requests.get(url)
+soup = BeautifulSoup(response.content, 'html.parser')
 
+episodeName = []
+episodeSeason = []
+rating = []
+reviewLink = []
+year = []
 
+episodeData = soup.findAll('div', attrs= {'class': 'list_item'})
 
+for i in episodeData:
+    episode = i.div.a.text.replace('\n','')
+    episodeSeason.append(episode)
 
+    episodeTitle = soup.strong.extract()
+    name = episodeTitle.text
+    episodeName.append(name)
 
+    date = i.div.find('div', class_ = 'airdate').text
+    year.append(date)
+
+print(episodeName)
+print(episodeSeason)
+print(year)
 
 print("\nProgram Terminated.\n")
