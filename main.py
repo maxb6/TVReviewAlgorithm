2155fb8acfdde6de64fc46f4addd5ba04ee8003c
maxb6 <madmax6b@gmail.com>
2021-06-15 10:41:17 -0400

    started extracting review data and counting word frequency

--- main.py
+++ main.py
@@ -6,6 +6,8 @@
 from matplotlib.pyplot import cla
 import pandas as pd
 import requests
+import string
+import re
 from bs4 import BeautifulSoup
 import numpy as np
 
@@ -21,13 +23,11 @@ class Episode:
         print(self.name, " ", self.season, " ", self.reviewLink, " ", self.year)
 
 
-
-
 url = 'https://www.imdb.com/title/tt0098904/episodes?season=1&ref_=ttep_ep_sn_pv'
 response = requests.get(url)
 soup = BeautifulSoup(response.content, 'html.parser')
 
-episodes=[]
+episodes = []
 episodeData = soup.findAll('div', attrs={'class': 'list_item'})
 date = soup.findAll("div", {"class": "airdate"})
 print(date)
@@ -38,13 +38,6 @@ for i in episodeData:
     episodeTitle = soup.strong.extract()
     name = episodeTitle.text
     print(name)
-   
-
-
-
-
-
-
 
 '''
 episodeName = []
@@ -70,5 +63,63 @@ print(episodeName)
 print(episodeSeason)
 print(years)
 '''
+
+
+######### Extract the Data and Build the Model  ###########
+# Using words for tokens in this model
+# Method to count word frequency for a review
+def wordFrequency(paragraph):
+    wordList = paragraph.split()
+    wordFreq = []
+    for w in wordList:
+        wordFreq.append(wordList.count(w))
+    print("Review\n" + paragraph)
+    print("List\n" + str(wordList) + "\n")
+    print("Frequencies\n" + str(wordFreq) + "\n")
+    print("Pairs\n" + str(list(zip(wordList, wordFreq))))
+
+
+def wordFrequency1(wordList):
+    wordFreq = []
+    for w in wordList:
+        wordFreq.append(wordList.count(w))
+    print("List\n" + str(wordList) + "\n")
+    print("Frequencies\n" + str(wordFreq) + "\n")
+    print("Pairs\n" + str(list(zip(wordList, wordFreq))))
+
+
+# method to remove stopwords from a given paragraph
+
+def removeStopwords(paragraph, stopwords):
+    wordList = paragraph.split()
+    resultWords = [word for word in wordList if word not in stopwords]
+    result = ' '.join(resultWords)
+    return result
+
+
+# Read the stopword file into a stopWordList
+stopWordFile = open("Stopword_File.txt", "r")
+stopWordList = stopWordFile.read()
+stopWordFile.close()
+print(stopWordList)
+
+# Extract the review data given a review URL of an episode
+url1 = 'https://www.imdb.com/title/tt0098286/reviews/?ref_=tt_ql_urv'
+response1 = requests.get(url1)
+soup1 = BeautifulSoup(response1.content, 'html.parser')
+# lister-item-content can be used to find review ratings
+reviewData = soup1.findAll('div', attrs={'class': 'content'})
+# iterate through all reviews of the season, clean the string and count the word frequency for each word
+for i in reviewData:
+    review = i.div.text
+    review = review.lower()
+    # find word count
+    wordCount = len(review.split())
+    print(wordCount)
+    # clean review by inserting <s>
+    cleanReview = review.replace(".", " <s>")
+    cleanReview = "<s> " + cleanReview
+    finalReview = removeStopwords(cleanReview, stopWordList)
+    wordFrequency(finalReview)
 
 print("\nProgram Terminated.\n")
